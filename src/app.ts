5df79bea730d1a26346af62878d0141d1b179544
Salman Farsi <salmanfarsi9005@gmail.com>
2026-01-12 00:47:30 +0600

    feat: add schema, controller and routes for user endpoints

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, { Application } from "express";
 import dotenv from "dotenv";
 import genreRoutes from "./routes/genre.routes";
 import bookRoutes from "./routes/book.routes";
+import authRoutes from "./routes/auth.routes";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.use("/api/genres", genreRoutes);
 app.use("/api/books", bookRoutes);
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("BookWorm server is running!");
---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import User, { IUser } from "../models/User";
+
+// Register
+export const registerUser = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User created successfully",
+      user: { id: newUser._id, name: newUser.name, email: newUser.email },
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    res.status(200).json({
+      message: "Login successful",
+      user: { id: user._id, name: user.name, email: user.email },
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
