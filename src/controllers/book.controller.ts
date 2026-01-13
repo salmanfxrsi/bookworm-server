dcb3559dc9bf08cb4174c66fe18e66a32d3cba79
Salman Farsi <salmanfarsi9005@gmail.com>
2026-01-13 16:29:33 +0600

    feat: add getBookById endpoint

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -1,25 +1,33 @@
 import { Request, Response } from "express";
 import Book from "../models/Book";
 import Genre from "../models/Genre";
+import mongoose from "mongoose";
+
 
 export const getBooks = async (req: Request, res: Response) => {
   try {
-    let { search = "", page = "1", limit = "5" } = req.query;
+    let { search = "", genre, page = "1", limit = "5" } = req.query;
 
     const pageNum = parseInt(page as string) || 1;
     const limitNum = parseInt(limit as string) || 5;
     const skip = (pageNum - 1) * limitNum;
 
     const filter: any = {};
-    if (search)
+
+    if (search) {
       filter.$or = [
         { title: { $regex: search as string, $options: "i" } },
         { author: { $regex: search as string, $options: "i" } },
       ];
+    }
+
+    if (genre) {
+      filter.genre = new mongoose.Types.ObjectId(genre as string);
+    }
 
     const total = await Book.countDocuments(filter);
     const books = await Book.find(filter)
-      .populate("genre", "name")
+      .populate("genre", "name") 
       .skip(skip)
       .limit(limitNum);
 
@@ -30,10 +38,36 @@ export const getBooks = async (req: Request, res: Response) => {
       totalPages: Math.ceil(total / limitNum),
       data: books,
     });
-  } catch {
+  } catch (err) {
+    console.error(err);
     res.status(500).json({ message: "Server error" });
   }
 };
+
+
+export const getBookById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    if (!id || typeof id !== "string" || !mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "Invalid book ID" });
+    }
+
+    const book = await Book.findById(id).populate("genre", "name");
+
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    res.status(200).json(book);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+
 
 export const createBook = async (req: Request, res: Response) => {
   try {
--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -4,11 +4,13 @@ import {
   createBook,
   updateBook,
   deleteBook,
+  getBookById,
 } from "../controllers/book.controller";
 
 const router = Router();
 
 router.get("/", getBooks);
+router.get("/:id", getBookById);
 router.post("/", createBook);
 router.put("/:id", updateBook);
 router.delete("/:id", deleteBook);
