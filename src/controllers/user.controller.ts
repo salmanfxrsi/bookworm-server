bb8a49d549ed1f104972a77f46565aa558bb2aca
Salman Farsi <salmanfarsi9005@gmail.com>
2026-01-14 22:58:39 +0600

    feat: add endpoints in user routes

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -38,8 +38,7 @@ export const loginUser = async (req: Request, res: Response) => {
 
   try {
     const user = await User.findOne({ email }).select("+password");
-    if (!user)
-      return res.status(401).json({ message: "Invalid credentials" });
+    if (!user) return res.status(401).json({ message: "Invalid credentials" });
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
@@ -54,5 +53,46 @@ export const loginUser = async (req: Request, res: Response) => {
     });
   } catch (error) {
     res.status(500).json({ message: "Login failed" });
+  }
+};
+
+export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await User.find().select("-password");
+    res.status(200).json(users);
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// UPDATE USER ROLE
+export const updateUserRole = async (req: Request, res: Response) => {
+  const { role } = req.body;
+
+  if (!role) return res.status(400).json({ message: "Role is required" });
+
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      { role },
+      { new: true }
+    ).select("-password");
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json(user);
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// DELETE USER
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
   }
 };
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,18 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/user.controller";
+import {
+  registerUser,
+  loginUser,
+  getAllUsers,
+  updateUserRole,
+  deleteUser,
+} from "../controllers/user.controller";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/", getAllUsers);
+router.put("/:id/role", updateUserRole);
+router.delete("/:id", deleteUser);
 
 export default router;
